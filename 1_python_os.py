029279925f45fe3d370ebf16661b8c215fe0d856
Clinton-G <166076564+Clinton-G@users.noreply.github.com>
2024-06-21 17:25:58 -0500

    Add files via upload

--- 1_python_os.py
+++ 1_python_os.py
@@ -1,8 +1,26 @@
 import os
 
 def list_directory_contents(path):
-    pass
-        # List and print all files and subdirectories in the given path
+    try:
+        with os.scandir(path) as entries:
+            for entry in entries:
+                if entry.is_dir():
+                    print('Directory', entry.name)
+                elif entry.is_file():
+                    print('File:', entry.name)
+                else:
 
+                    print('Other:', entry.name)
 
-# I don't understand this assignment
+    except FileNotFoundError:
+        print('FileNotFoundError')
+
+    except Exception as e:
+        print('ExceptionError', e)
+
+def main():
+    path = input('Enter Directory Path: ')
+    list_directory_contents(path)
+
+if __name__ == '__main__':
+    main()
